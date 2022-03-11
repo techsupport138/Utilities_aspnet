1bfc49b6e51299983284e51e8a909ff92009b91b
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-03-11 09:56:02 +0330

    code clean

--- ServiceProvider/Entities/ServiceProviderEntity.cs
+++ ServiceProvider/Entities/ServiceProviderEntity.cs
@@ -4,19 +4,18 @@ using Utilities_aspnet.Utilities.Entities;
 
 namespace Utilities_aspnet.ServiceProvider.Entities;
 
-public class ServiceProviderEntity<TType> : BaseEntity {
+public class ServiceProviderEntity : BaseEntity {
     [Required]
-    public TType Type { get; set; } = default!;
+    public string Type { get; set; } = default!;
 
     [Required]
     public int Point { get; set; } = 0;
-    
+
     public double? Latitude { get; set; }
     public double? Longitude { get; set; }
     public string? Services { get; set; }
     public string? Address { get; set; }
 
-    
     public IEnumerable<MediaEntity>? Media { get; set; }
     public IEnumerable<ContactInformationEntity>? ContactInformation { get; set; }
     public IEnumerable<CommentEntity>? Comment { get; set; }
