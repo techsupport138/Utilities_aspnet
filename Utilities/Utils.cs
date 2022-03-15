6112ef03f2bb66d86b97a9b31335285271a482c4
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-03-15 13:27:05 +0330

    fix swagger

--- Utilities/Utils.cs
+++ Utilities/Utils.cs
@@ -5,18 +5,22 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Utilities_aspnet.Utilities;
 
 public static class StartupExtension {
+    static StartupExtension() { }
+
     public static void SetupUtilities<T>(this IServiceCollection services, string connectionStrings) where T : DbContext {
         services.AddUtilitiesServices<T>(connectionStrings);
         services.AddUtilitiesSwagger();
         services.AddUtilitiesIdentity();
     }
 
-    public static void AddUtilitiesServices<T>(this IServiceCollection services, string connectionStrings) where T : DbContext {
+    private static void AddUtilitiesServices<T>(this IServiceCollection services, string connectionStrings) where T : DbContext {
         services.AddScoped<DbContext, T>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.AddDbContext<T>(options => options.UseSqlServer(connectionStrings).EnableSensitiveDataLogging());
@@ -40,5 +44,15 @@ public static class StartupExtension {
         app.UseStaticFiles();
         app.UseAuthorization();
         app.UseRouting();
+    }
+
+    private static void AddUtilitiesSwagger(this IServiceCollection services) {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    private static void UseUtilitiesSwagger(this IApplicationBuilder app) {
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
 }
