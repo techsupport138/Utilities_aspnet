e4eb49492ed2d7b776740c6f75cd66ec9c0d57d4
Nima Ataee <nimasoftco@gmail.com>
2022-03-30 18:46:36 +0430

    commit sync 2

---
+++ User/Entities/UserRoleEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities_aspnet.Utilities.Entities;
+
+namespace Utilities_aspnet.User.Entities
+{
+    public class UserRoleEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+    }
+}
