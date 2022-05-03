78c83b623add2b6296c85fd4db835d616bf89be5
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-04 00:35:48 +0430

    add product

--- Product/Data/ProductRepository.cs
+++ Product/Data/ProductRepository.cs
@@ -1,13 +1,14 @@
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Utilities_aspnet.Product.Dto;
+using Utilities_aspnet.Utilities.Responses;
 
 namespace Utilities_aspnet.Product.Data;
 
 public interface IProductRepository<T> where T : BasePEntity {
-    Task<GetProductDto> Add(AddUpdateProductDto dto);
-    Task<IEnumerable<GetProductDto>> Get();
-    Task<GetProductDto> GetById(Guid id);
-    Task<GetProductDto> Update(Guid id, AddUpdateProductDto dto);
+    Task<GenericResponse<GetProductDto>> Add(AddUpdateProductDto dto);
+    Task<GenericResponse<IEnumerable<GetProductDto>>> Get();
+    Task<GenericResponse<GetProductDto>> GetById(Guid id);
+    Task<GenericResponse<GetProductDto>> Update(Guid id, AddUpdateProductDto dto);
     void Delete(Guid id);
 }
 
@@ -20,30 +21,30 @@ public class ProductRepository<T> : IProductRepository<T> where T : BasePEntity 
         _mapper = mapper;
     }
 
-    public async Task<GetProductDto> Add(AddUpdateProductDto dto) {
+    public async Task<GenericResponse<GetProductDto>> Add(AddUpdateProductDto dto) {
         if (dto == null) throw new ArgumentException("Dto must not be null", nameof(dto));
         EntityEntry<T> i = await _dbContext.Set<T>().AddAsync(_mapper.Map<T>(dto));
         await _dbContext.SaveChangesAsync();
-        return _mapper.Map<GetProductDto>(i.Entity);
+        return new GenericResponse<GetProductDto>(_mapper.Map<GetProductDto>(i.Entity));
     }
 
-    public async Task<IEnumerable<GetProductDto>> Get() {
+    public async Task<GenericResponse<IEnumerable<GetProductDto>>> Get() {
         List<T> i = await _dbContext.Set<T>().AsNoTracking().ToListAsync();
-        return _mapper.Map<IEnumerable<GetProductDto>>(i);
+        return new GenericResponse<IEnumerable<GetProductDto>>(_mapper.Map<IEnumerable<GetProductDto>>(i));
     }
 
-    public async Task<GetProductDto> GetById(Guid id) {
+    public async Task<GenericResponse<GetProductDto>> GetById(Guid id) {
         T? i = await _dbContext.Set<T>().AsNoTracking().FirstOrDefaultAsync(i => i.Id == id);
-        return _mapper.Map<GetProductDto>(i);
+        return new GenericResponse<GetProductDto>(_mapper.Map<GetProductDto>(i));
     }
 
-    public Task<GetProductDto> Update(Guid id, AddUpdateProductDto dto) {
+    public Task<GenericResponse<GetProductDto>> Update(Guid id, AddUpdateProductDto dto) {
         throw new NotImplementedException();
     }
 
     public async void Delete(Guid id) {
-        GetProductDto i = await GetById(id);
-        _dbContext.Set<T>().Remove(_mapper.Map<T>(i));
+        GenericResponse<GetProductDto> i = await GetById(id);
+        _dbContext.Set<T>().Remove(_mapper.Map<T>(i.Result));
         await _dbContext.SaveChangesAsync();
     }
 }
