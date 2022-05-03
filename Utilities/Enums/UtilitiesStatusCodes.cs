5d9e74e03f3a9b7a01ee27cb15aa0fc5c9500744
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-03 17:16:44 +0430

    add status code extension

--- Utilities/Enums/UtilitiesStatusCodes.cs
+++ Utilities/Enums/UtilitiesStatusCodes.cs
@@ -1,11 +1,17 @@
 namespace Utilities_aspnet.Utilities.Enums;
 
-public enum UtilitiesStatusCodes
-{
+public enum UtilitiesStatusCodes {
     Success = 200,
     BadRequest = 400,
     Forbidden = 403,
     NotFound = 404,
     Unhandled = 900,
-    New=499
+    New = 499
+}
+
+public static class UtilitiesStatusCodesExtension {
+    public static bool isSuccessful(this UtilitiesStatusCodes statusCode) => statusCode >= UtilitiesStatusCodes.Success;
+    public static bool isBadRequest(this UtilitiesStatusCodes statusCode) => statusCode >= UtilitiesStatusCodes.BadRequest;
+    public static bool isForbidden(this UtilitiesStatusCodes statusCode) => statusCode >= UtilitiesStatusCodes.Forbidden;
+    public static bool isNotFound(this UtilitiesStatusCodes statusCode) => statusCode >= UtilitiesStatusCodes.NotFound;
 }
