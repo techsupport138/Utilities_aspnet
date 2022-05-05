daa08b5f34e4c3f656745fff7ad1deca330b1c3d
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-05 13:32:13 +0430

    add GetCategoryDto file

---
+++ Base/Dtos/GetCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Utilities_aspnet.Base.Dtos;
+
+public class GetCategoryDto
+{
+    public Guid? CategoryId { get; set; }
+    public Guid? ParentId { get; set; }
+    public Guid? MediaId { get; set; }
+    public string LanguageId { get; set; } = "fa-IR";
+    public CategoryForEnum CategoryFor { get; set; }
+    public string Title { get; set; }
+}
