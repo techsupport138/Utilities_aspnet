02d34c0758d96798077e723aa3768acad5734315
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-05 14:56:13 +0430

    code clean

--- Product/Data/ProductRepository.cs
+++ Product/Data/ProductRepository.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Utilities_aspnet.Product.Dto;
 using Utilities_aspnet.Utilities.Responses;
+using Xamarin.Forms.Internals;
 
 namespace Utilities_aspnet.Product.Data;
 
--- Product/Dto/ProductDto.cs
+++ Product/Dto/ProductDto.cs
@@ -3,19 +3,19 @@ using Utilities_aspnet.User.Dtos;
 namespace Utilities_aspnet.Product.Dto;
 
 public class GetProductDto {
-    public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Subtitle { get; set; }
-    public string Description { get; set; }
-    public bool Enabled { get; set; }
+    public Guid? Id { get; set; }
+    public string? Title { get; set; }
+    public string? Subtitle { get; set; }
+    public string? Description { get; set; }
     public string? SubTitle { get; set; }
-    public decimal Price { get; set; }
-    public bool IsForSale { get; set; }
-    public bool IsBookmarked { get; set; }
-    public int VisitsCount { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public bool? Enabled { get; set; }
+    public bool? IsForSale { get; set; }
+    public bool? IsBookmarked { get; set; }
+    public decimal? Price { get; set; }
+    public int? VisitsCount { get; set; }
+    public DateTime? CreatedAt { get; set; }
     public UserReadDto? User { get; set; }
-    public LocationReadDto Location { get; set; }
+    public LocationReadDto? Location { get; set; }
     public IEnumerable<MediaDto>? Media { get; set; }
     public IEnumerable<IdTitleDto>? Categories { get; set; }
     public IEnumerable<IdTitleDto>? Team { get; set; }
