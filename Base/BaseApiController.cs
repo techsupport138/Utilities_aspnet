fc1c0b34d5da4257b9074b719432b836ac9a32f1
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-10 22:22:02 +0430

    add base api controller

--- Base/BaseApiController.cs
+++ Base/BaseApiController.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Utilities_aspnet.Utilities.Responses;
 
 namespace Utilities_aspnet.Base;
 
 [ApiController]
-public class BaseApiController : ControllerBase { }
+public class BaseApiController : ControllerBase {
+    [NonAction]
+    public ObjectResult Result(GenericResponse response) => new(response) {
+        StatusCode = response.Status.value()
+    };
+}
