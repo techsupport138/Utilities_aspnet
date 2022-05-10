0c325ec2257bf3aed8cca038793fe20797c34c55
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-10 21:12:36 +0430

    add api controllers

---
+++ ApiControllers/BaseApiController.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Utilities_aspnet.ApiControllers {
+    public class BaseApiController : ControllerBase { }
+}
---
+++ ApiControllers/EnumController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Utilities_aspnet.Utilities.Data;
+using Utilities_aspnet.Utilities.Responses;
+
+namespace Utilities_aspnet.ApiControllers {
+    [ApiController]
+    [Route("api/enum")]
+    public class EnumController : BaseApiController {
+        private readonly IEnumRepository _enumRepository;
+
+        public EnumController(IEnumRepository enumRepository) {
+            _enumRepository = enumRepository;
+        }
+
+        [HttpGet(nameof(GetAll))]
+        public async Task<ActionResult<GenericResponse<EnumDto>>> GetAll(bool showCategory = false,
+            bool showGeo = false) {
+            GenericResponse? i = await _enumRepository.GetAll(showCategory, showGeo);
+            return Ok(i);
+        }
+    }
+}
