016ae117ced792b94c595a056a69344f9abe8609
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-13 22:22:51 +0430

    code clean

--- Utilities/Core/BaseApiController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-
-
-namespace Utilities_aspnet.Utilities.Core;
-
-[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-public class BaseApiController : ControllerBase { }
