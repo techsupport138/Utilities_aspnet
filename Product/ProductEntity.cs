32449357f8ec6b067b9edfbde0f4be8ea5b52375
Poya-darbandi75 <44476275+Poya-darbandi75@users.noreply.github.com>
2022-05-15 09:15:01 +0430

    Nullable Vote, Report In ProductEntity

--- Product/ProductEntity.cs
+++ Product/ProductEntity.cs
@@ -22,8 +22,8 @@ public abstract class BaseProductEntity : BaseEntity {
     public IEnumerable<MediaEntity>? Media { get; set; }
     public IEnumerable<CategoryEntity>? Category { get; set; }
     public IEnumerable<TagEntity>? Tag { get; set; }
-    public IEnumerable<VoteFieldEntity> VoteField { get; set; }
-    public IEnumerable<ReportEntity> Report { get; set; }
+    public IEnumerable<VoteFieldEntity>? VoteField { get; set; }
+    public IEnumerable<ReportEntity>? Report { get; set; }
 
     public static ProductReadDto MapReadDto(ProductEntity e) {
         ProductReadDto dto = new ProductReadDto {
