29d77578ac45287d99dea282d3b7d96c0d489646
Poya-darbandi75 <44476275+Poya-darbandi75@users.noreply.github.com>
2022-05-26 21:00:32 +0430

    Fix Read UserId From Token

--- Product/ProductRepository.cs
+++ Product/ProductRepository.cs
@@ -1,7 +1,7 @@
 namespace Utilities_aspnet.Product;
 
 public interface IProductRepository<T> where T : BaseProductEntity {
-    Task<GenericResponse<ProductReadDto>> Create(ProductCreateUpdateDto dto, string userId);
+    Task<GenericResponse<ProductReadDto>> Create(ProductCreateUpdateDto dto);
     Task<GenericResponse<IEnumerable<ProductReadDto>>> Read();
     Task<GenericResponse<ProductReadDto>> ReadById(Guid id);
     Task<GenericResponse<ProductReadDto>> Update(ProductCreateUpdateDto dto);
@@ -19,12 +19,11 @@ public class ProductRepository<T> : IProductRepository<T> where T : BaseProductE
         _httpContextAccessor = httpContextAccessor;
     }
 
-    public async Task<GenericResponse<ProductReadDto>> Create(ProductCreateUpdateDto dto, string userId) {
+    public async Task<GenericResponse<ProductReadDto>> Create(ProductCreateUpdateDto dto) {
         if (dto == null) throw new ArgumentException("Dto must not be null", nameof(dto));
         T entity = _mapper.Map<T>(dto);
 
-        //entity.UserId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        entity.UserId = userId;
+        entity.UserId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         List<ReferenceEntity> references = new();
         List<BrandEntity> brands = new();
         List<CategoryEntity> categories = new();
