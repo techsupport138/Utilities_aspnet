65710d9edeb92a9ab622adaceaf215d2eac1abe9
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2022-05-27 10:59:02 +0430

    remove followe and following from user

--- User/Entities/UserEntity.cs
+++ User/Entities/UserEntity.cs
@@ -23,8 +23,8 @@ public class UserEntity : IdentityUser
         Services = new List<ServiceEntity>();
         Magazines = new List<MagazineEntity>();
         Specialties = new List<SpecialityEntity>();
-        Follower = new List<FollowEntity>();
-        Following = new List<FollowEntity>();
+        //Follower = new List<FollowEntity>();
+        //Following = new List<FollowEntity>();
     }
 
     public bool Suspend { get; set; }
@@ -38,8 +38,8 @@ public class UserEntity : IdentityUser
     public double Wallet { get; set; } = 0;
     public DateTime Birthdate { get; set; }
 
-    public List<FollowEntity> Follower { get; set; }
-    public List<FollowEntity> Following { get; set; }
+    //public List<FollowEntity> Follower { get; set; }
+    //public List<FollowEntity> Following { get; set; }
     public List<LocationEntity> Location { get; set; }
     public List<FormEntity> FormBuilders { get; set; }
     public List<MediaEntity> Media { get; set; }
