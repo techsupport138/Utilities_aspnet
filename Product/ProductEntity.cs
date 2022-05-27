000b1fbda065b3d578f181d73a0567aa5e8facf6
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-27 15:06:49 +0430

    fox product dto

--- Product/ProductEntity.cs
+++ Product/ProductEntity.cs
@@ -56,7 +56,11 @@ public class ServiceEntity : BaseProductEntity { }
 [Table("Magazine")]
 public class MagazineEntity : BaseProductEntity { }
 
-public class ProductReadDto : BaseReadDto {
+public class ProductReadDto {
+    public Guid? Id { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
     public string? Title { get; set; }
     public string? Subtitle { get; set; }
     public string? Description { get; set; }
