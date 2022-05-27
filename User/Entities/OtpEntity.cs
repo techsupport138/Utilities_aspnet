9792ba4a0201ebf188449245b9141526f451ba0a
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-27 11:00:32 +0430

    fix user

--- User/Entities/OtpEntity.cs
+++ User/Entities/OtpEntity.cs
@@ -1,14 +1,8 @@
 namespace Utilities_aspnet.User.Entities;
 
 public class OtpEntity : BaseEntity {
-    [Required]
-    [StringLength(4)]
     public string OtpCode { get; set; }
 
     public UserEntity User { get; set; }
-
-    [Required]
-    [StringLength(450)]
-    [ForeignKey(nameof(User))]
     public string UserId { get; set; }
 }
--- User/Entities/UserEntity.cs
+++ User/Entities/UserEntity.cs
@@ -3,38 +3,16 @@
 namespace Utilities_aspnet.User.Entities;
 
 public class UserEntity : IdentityUser {
-    public UserEntity() {
-        Location = new List<LocationEntity>();
-        FormBuilders = new List<FormEntity>();
-        Media = new List<MediaEntity>();
-        Colors = new List<ColorEntity>();
-        Favorites = new List<FavoriteEntity>();
-        ContactInformation = new List<ContactInformationEntity>();
-        Reports = new List<ReportEntity>();
-        Products = new List<ProductEntity>();
-        Projects = new List<ProjectEntity>();
-        Tutorials = new List<TutorialEntity>();
-        Events = new List<EventEntity>();
-        Ads = new List<AdEntity>();
-        Companys = new List<CompanyEntity>();
-        Tenders = new List<TenderEntity>();
-        Services = new List<ServiceEntity>();
-        Magazines = new List<MagazineEntity>();
-        Specialties = new List<SpecialityEntity>();
-        Follower = new List<FollowEntity>();
-        Following = new List<FollowEntity>();
-    }
-
     public bool Suspend { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? FullName { get; set; }
     public string? Headline { get; set; }
     public string? Bio { get; set; }
-    public string AppUserName { get; set; }
-    public string AppPhoneNumber { get; set; }
-    public double Wallet { get; set; } = 0;
-    public DateTime Birthdate { get; set; }
+    public string? AppUserName { get; set; }
+    public string? AppPhoneNumber { get; set; }
+    public double? Wallet { get; set; } = 0;
+    public DateTime? Birthdate { get; set; }
 
     public List<FollowEntity>? Follower { get; set; }
     public List<FollowEntity>? Following { get; set; }
@@ -43,16 +21,16 @@ public class UserEntity : IdentityUser {
     public List<MediaEntity>? Media { get; set; }
     public List<ColorEntity>? Colors { get; set; }
     public List<FavoriteEntity>? Favorites { get; set; }
-    public List<ContactInformationEntity> ContactInformation { get; set; }
-    public List<ReportEntity> Reports { get; set; }
-    public List<ProductEntity> Products { get; set; }
-    public List<ProjectEntity> Projects { get; set; }
-    public List<TutorialEntity> Tutorials { get; set; }
-    public List<EventEntity> Events { get; set; }
-    public List<AdEntity> Ads { get; set; }
-    public List<CompanyEntity> Companys { get; set; }
-    public List<TenderEntity> Tenders { get; set; }
-    public List<ServiceEntity> Services { get; set; }
-    public List<MagazineEntity> Magazines { get; set; }
-    public List<SpecialityEntity> Specialties { get; set; }
+    public List<ContactInformationEntity>? ContactInformation { get; set; }
+    public List<ReportEntity>? Reports { get; set; }
+    public List<ProductEntity>? Products { get; set; }
+    public List<ProjectEntity>? Projects { get; set; }
+    public List<TutorialEntity>? Tutorials { get; set; }
+    public List<EventEntity>? Events { get; set; }
+    public List<AdEntity>? Ads { get; set; }
+    public List<CompanyEntity>? Companys { get; set; }
+    public List<TenderEntity>? Tenders { get; set; }
+    public List<ServiceEntity>? Services { get; set; }
+    public List<MagazineEntity>? Magazines { get; set; }
+    public List<SpecialityEntity>? Specialties { get; set; }
 }
