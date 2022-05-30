d8065b3d8777ac27a2ec6829e78f27b4bb5bbdba
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2022-05-30 23:08:11 +0430

    Update BookmarkController.cs

--- Controllers/BookmarkController.cs
+++ Controllers/BookmarkController.cs
@@ -3,19 +3,30 @@ using Utilities_aspnet.FollowBookmark;
 
 namespace Utilities_aspnet.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/[controller]/[action]")]
 [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 [ApiController]
-public class BookmarkController : BaseApiController {
+public class BookmarkController : BaseApiController
+{
     private readonly IFollowBookmarkRepository _repository;
 
-    public BookmarkController(IFollowBookmarkRepository repository) {
+    public BookmarkController(IFollowBookmarkRepository repository)
+    {
         _repository = repository;
     }
 
-    [HttpPost("{id:guid}")]
-    public async Task<IActionResult> ToggleBookmark(Guid id) {
-        GenericResponse i = await _repository.ToggleBookmark(id);
+    [HttpPost]
+    public async Task<IActionResult> ToggleBookmark(BookmarkWriteDto parameters)
+    {
+        GenericResponse i = await _repository.ToggleBookmark(parameters);
         return Result(i);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<GenericResponse<IEnumerable<BookmarkEntity>>>> GetBookMarks()
+    {
+        var result = await _repository.GetBookMarks();
+
+        return Result(result);
     }
 }
