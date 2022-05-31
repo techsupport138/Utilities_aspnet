19ad0d42842d2c7ca773ad5f61cea15f5ab6e1c0
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-31 10:46:49 +0430

    add product controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+namespace Utilities_aspnet.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : BaseApiController {
+    private readonly IProductRepository<ProductEntity> _productRepository;
+
+    public ProductController(IProductRepository<ProductEntity> productRepository) {
+        _productRepository = productRepository;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<GenericResponse<ProductReadDto>>> Create(ProductCreateUpdateDto dto) {
+        GenericResponse<ProductReadDto> i = await _productRepository.Create(dto);
+        return Result(i);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<GenericResponse<IEnumerable<ProductReadDto>>>> Read(FilterProductDto? dto) {
+        GenericResponse<IEnumerable<ProductReadDto>> i = await _productRepository.Read(dto);
+        return Result(i);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<GenericResponse<ProductReadDto>>> ReadById(Guid id) {
+        GenericResponse<ProductReadDto> i = await _productRepository.ReadById(id);
+        return Result(i);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<GenericResponse<ProductReadDto>>> Update(ProductCreateUpdateDto dto) {
+        GenericResponse<ProductReadDto> i = await _productRepository.Update(dto);
+        return Result(i);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id) {
+        await _productRepository.Delete(id);
+        return Ok();
+    }
+}
