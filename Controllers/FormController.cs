37177d501b805aed2dbf4ffbcb592f105b58f258
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-05-31 13:53:08 +0430

    add form controller

---
+++ Controllers/FormController.cs
@@ -0,0 +1,29 @@
+namespace Utilities_aspnet.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FormController : BaseApiController {
+    private readonly IFormRepository _formRepository;
+
+    public FormController(IFormRepository formRepository) {
+        _formRepository = formRepository;
+    }
+
+    [HttpPost("CreateFormField")]
+    public async Task<ActionResult<GenericResponse<List<FormFieldDto>>>> CreateFormField(FormFieldDto dto) {
+        GenericResponse<List<FormFieldDto>?> i = await _formRepository.CreateFormFields(dto);
+        return Result(i);
+    }
+
+    [HttpGet("{categoryId:guid}")]
+    public async Task<ActionResult<GenericResponse<List<FormFieldDto>>>> ReadFormFieldById(Guid categoryId) {
+        GenericResponse<List<FormFieldDto>> i = await _formRepository.ReadFormFields(categoryId);
+        return Result(i);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<GenericResponse<List<FormFieldDto>>>> CreateForm(FormCreateDto model) {
+        GenericResponse<List<FormFieldDto>> i = await _formRepository.UpdateFormBuilder(model);
+        return Result(i);
+    }
+}
