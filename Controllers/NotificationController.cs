9e4f407b22ede74a136df0f6779ba800cbdd799f
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-06-01 11:47:00 +0430

    fix NotificationController

--- Controllers/NotificationController.cs
+++ Controllers/NotificationController.cs
@@ -1,8 +1,8 @@
 namespace Utilities_aspnet.Controllers;
 
 [ApiController]
-[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 [Route("api/[controller]")]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 public class NotificationController : BaseApiController {
     private readonly INotificationRepository _notificationRepository;
 
@@ -11,16 +11,13 @@ public class NotificationController : BaseApiController {
     }
 
     [HttpGet]
-    public async Task<ActionResult<GenericResponse<IEnumerable<NotificationDto>>>> GetConversations()
-    {
+    public async Task<ActionResult<GenericResponse<IEnumerable<NotificationDto>>>> Read() {
         GenericResponse<IEnumerable<NotificationDto>> i = await _notificationRepository.GetNotifications();
         return Result(i);
     }
 
-
     [HttpPost]
-    public async Task<ActionResult<GenericResponse>> PostConversation(CreateNotificationDto model)
-    {
+    public async Task<ActionResult<GenericResponse>> Create(CreateNotificationDto model) {
         GenericResponse i = await _notificationRepository.CreateNotification(model);
         return Result(i);
     }
