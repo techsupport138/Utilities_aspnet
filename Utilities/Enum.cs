8d05cc11fc31a62900f77a0928221179d7098674
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-06-11 21:17:33 +0430

    add properties

--- Utilities/Enum.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Utilities_aspnet.Utilities;
-
-public static class EnumExtension {
-    public static List<IdTitleReadDto> GetValues<T>() {
-        return (from int itemType in Enum.GetValues(typeof(T))
-            select new IdTitleReadDto {Title = Enum.GetName(typeof(T), itemType), SecondaryId = itemType}).ToList();
-    }
-}
