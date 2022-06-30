6031c76d2c50c71707cffeb3b6c5178f8a564ece
Poya-darbandi75 <44476275+Poya-darbandi75@users.noreply.github.com>
2022-06-30 17:58:55 +0430

    Sort Comment

--- Repositories/CommentRepository.cs
+++ Repositories/CommentRepository.cs
@@ -25,7 +25,7 @@ public class CommentRepository : ICommentRepository {
 			.Include(x=>x.Media)
 			.Include(x=>x.Children)!.ThenInclude(x=>x.User)!.ThenInclude(x => x.Media)
 			.Include(x => x.Children)!.ThenInclude(x=>x.Children)
-			.Where(x => x.ProductId == id && x.ParentId == null).OrderBy(x=>x.CreatedAt).ToListAsync();
+			.Where(x => x.ProductId == id && x.ParentId == null).OrderByDescending(x=>x.CreatedAt).ToListAsync();
 
 		IEnumerable<CommentReadDto>? result = _mapper.Map<IEnumerable<CommentReadDto>?>(comment);
 
