6f7555c10b8e82fed1ffca363ca789ec648e8ab2
Poya-darbandi75 <44476275+Poya-darbandi75@users.noreply.github.com>
2022-07-06 11:15:51 +0430

    Add UseCase Follow For Notification

--- Repositories/FollowBookmarkRepository.cs
+++ Repositories/FollowBookmarkRepository.cs
@@ -164,7 +164,7 @@ public class FollowBookmarkRepository : IFollowBookmarkRepository {
 			await _context.SaveChangesAsync();
 			try {
 				_notificationRepository.CreateNotification(new NotificationCreateUpdateDto {
-					UserId = parameters.UserId, Message = "You are being followed by " + myUser.UserName, Title = "Follow"
+					UserId = parameters.UserId, Message = "You are being followed by " + myUser.UserName, Title = "Follow", UseCase = "Follow"
 				});
 			}
 			catch { }
