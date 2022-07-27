3a9b807c2bfe622dccc21fccd24e10ce8281ddb9
Poya-darbandi75 <44476275+Poya-darbandi75@users.noreply.github.com>
2022-07-27 11:54:26 +0430

    Add Category In Notification

--- Repositories/NotificationRepository.cs
+++ Repositories/NotificationRepository.cs
@@ -19,7 +19,7 @@ public class NotificationRepository : INotificationRepository {
 	public async Task<GenericResponse<IEnumerable<NotificationDto>>> GetNotifications() {
 		string? userId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 		IEnumerable<NotificationEntity> model = await _context.Set<NotificationEntity>().Include(x => x.Media)
-			.Include(x => x.CreatorUser).ThenInclude(x => x.Media)
+			.Include(x => x.CreatorUser).ThenInclude(x => x.Media).Include(x => x.CreatorUser).ThenInclude(x => x.Categories)
 			.Where(x => (x.UserId == null || x.UserId == userId) && x.DeletedAt == null).OrderByDescending(x => x.CreatedAt)
 			.ToListAsync();
 
