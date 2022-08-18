70af15125f0defa793199731885b24f56614cd16
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-08-18 17:17:01 +0430

    fix media

--- Entities/MediaEntity.cs
+++ Entities/MediaEntity.cs
@@ -8,22 +8,49 @@ public class MediaEntity : BaseEntity {
 	public string? Title { get; set; }
 	public VisibilityType? Visibility { get; set; } = VisibilityType.Public;
 
+	[NotMapped]
+	public string Url => $"{Server.ServerAddress}/Medias/{FileName}";
+
+	[System.Text.Json.Serialization.JsonIgnore]
 	public ContentEntity? Content { get; set; }
+
+	[System.Text.Json.Serialization.JsonIgnore]
 	public Guid? ContentId { get; set; }
 
+	[System.Text.Json.Serialization.JsonIgnore]
 	public UserEntity? User { get; set; }
+
+	[System.Text.Json.Serialization.JsonIgnore]
 	public string? UserId { get; set; }
 
+	[System.Text.Json.Serialization.JsonIgnore]
 	public ProductEntity? Product { get; set; }
+
+	[System.Text.Json.Serialization.JsonIgnore]
 	public Guid? ProductId { get; set; }
+
+	[System.Text.Json.Serialization.JsonIgnore]
 	public CommentEntity? Comment { get; set; }
+
+	[System.Text.Json.Serialization.JsonIgnore]
 	public Guid? CommentId { get; set; }
+
+	[System.Text.Json.Serialization.JsonIgnore]
 	public ChatEntity? Chat { get; set; }
+
+	[System.Text.Json.Serialization.JsonIgnore]
 	public Guid? ChatId { get; set; }
 
+	[System.Text.Json.Serialization.JsonIgnore]
 	public NotificationEntity? Notification { get; set; }
+
+	[System.Text.Json.Serialization.JsonIgnore]
 	public Guid? NotificationId { get; set; }
+
+	[System.Text.Json.Serialization.JsonIgnore]
 	public CategoryEntity? Category { get; set; }
+
+	[System.Text.Json.Serialization.JsonIgnore]
 	public Guid? CategoryId { get; set; }
 }
 
