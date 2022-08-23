bedf0c245a5f3b5fff6eb957ca712977b64ec751
Neda Mohammadi <neda.mohammadi88@gmail.com>
2022-08-23 23:13:37 +0430

    isfollowing in notification

--- Entities/NotificationEntity.cs
+++ Entities/NotificationEntity.cs
@@ -1,44 +1,52 @@
 ﻿namespace Utilities_aspnet.Entities;
 
 [Table("Notifications")]
-public class NotificationEntity : BaseEntity {
-	public string? Title { get; set; } = null!;
-	public string? Message { get; set; }
-	public string? Link { get; set; }
-	public string? UseCase { get; set; }
-	public bool? Visited { get; set; }
-	public IEnumerable<MediaEntity>? Media { get; set; }
+public class NotificationEntity : BaseEntity
+{
+    public string? Title { get; set; } = null!;
+    public string? Message { get; set; }
+    public string? Link { get; set; }
+    public string? UseCase { get; set; }
+    public bool? Visited { get; set; }
+    public IEnumerable<MediaEntity>? Media { get; set; }
 
-	public string? UserId { get; set; }
+    public string? UserId { get; set; }
 
-	[ForeignKey(nameof(UserId))]
-	public UserEntity? User { get; set; }
+    [ForeignKey(nameof(UserId))]
+    public UserEntity? User { get; set; }
 
-	public string? CreatorUserId { get; set; }
+    public string? CreatorUserId { get; set; }
 
-	[ForeignKey(nameof(CreatorUserId))]
-	public UserEntity? CreatorUser { get; set; }
+    [ForeignKey(nameof(CreatorUserId))]
+    public UserEntity? CreatorUser { get; set; }
+
+    [NotMapped]
+    public bool IsFollowing { get; set; } = false;
+
 }
 
-public class NotificationDto {
-	public Guid? Id { get; set; }
-	public string? Title { get; set; }
-	public string? Message { get; set; }
-	public string? Link { get; set; }
-	public DateTime CreatedAt { get; set; }
-	public bool Visited { get; set; }
-	public string? UseCase { get; set; }
-	public UserReadDto? CreatorUser { get; set; }
-	public IEnumerable<MediaEntity>? Media { get; set; }
+public class NotificationDto
+{
+    public Guid? Id { get; set; }
+    public string? Title { get; set; }
+    public string? Message { get; set; }
+    public string? Link { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool Visited { get; set; }
+    public string? UseCase { get; set; }
+    public UserReadDto? CreatorUser { get; set; }
+    public IEnumerable<MediaEntity>? Media { get; set; }
+    public bool IsFollowing { get; set; } = false;
 }
 
-public class NotificationCreateUpdateDto {
-	public Guid? Id { get; set; } = null!;
-	public string? Title { get; set; } = null!;
-	public string? UserId { get; set; }
-	public string? CreatorUserId { get; set; }
-	public string? Message { get; set; }
-	public string? Link { get; set; }
-	public string? Media { get; set; }
-	public string? UseCase { get; set; }
+public class NotificationCreateUpdateDto
+{
+    public Guid? Id { get; set; } = null!;
+    public string? Title { get; set; } = null!;
+    public string? UserId { get; set; }
+    public string? CreatorUserId { get; set; }
+    public string? Message { get; set; }
+    public string? Link { get; set; }
+    public string? Media { get; set; }
+    public string? UseCase { get; set; }
 }
--- Repositories/NotificationRepository.cs
+++ Repositories/NotificationRepository.cs
@@ -16,15 +16,24 @@ public class NotificationRepository : INotificationRepository {
 		_httpContextAccessor = httpContextAccessor;
 	}
 
-	public async Task<GenericResponse<IEnumerable<NotificationDto>>> GetNotifications() {
+	public async Task<GenericResponse<IEnumerable<NotificationDto>>> GetNotifications() 
+	{
 		string? userId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
 		IEnumerable<NotificationEntity> model = await _context.Set<NotificationEntity>().Include(x => x.Media)
 			.Include(x => x.CreatorUser).ThenInclude(x => x.Media).Include(x => x.CreatorUser).ThenInclude(x => x.Categories)
 			.Where(x => (x.UserId == null || x.UserId == userId) && x.DeletedAt == null).OrderByDescending(x => x.CreatedAt)
 			.ToListAsync();
 
-		IEnumerable<NotificationDto> notificationDtos = _mapper.Map<IEnumerable<NotificationDto>>(model);
+		if (userId != null)
+		{
+			foreach (NotificationEntity item in model)
+			{
+				item.IsFollowing = await _context.Set<FollowEntity>().AnyAsync(x => x.FollowsUserId == userId && x.FollowerUserId == item.UserId);
+			}
+		}
 
+		IEnumerable<NotificationDto> notificationDtos = _mapper.Map<IEnumerable<NotificationDto>>(model);
 		return new GenericResponse<IEnumerable<NotificationDto>>(notificationDtos);
 	}
 
